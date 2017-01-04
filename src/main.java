3b3954bd21e401e509383445eb21c9311d83a1d6
TasfiqulGhani <tasfiqulghani@gmail.com>
2017-01-04 22:45:41 +0600

    code

---
+++ src/main.java
@@ -0,0 +1,23 @@
+
+public class main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.print("Before for loop\n");
+		
+		for(int i=0;i<3;i++){
+
+			System.out.print("Tashfik\n");
+			
+		}
+		
+		System.out.print("After for loop\n");
+		
+
+	}
+
+}
